05bff769f1b6f59e90949714fe5d5be9b7db51c2
Benedict Kioko <benedictmkioko@gmail.com>
2022-02-15 21:07:34 -0500

    Add login and signup pages

--- routes/main.js
+++ routes/main.js
@@ -6,4 +6,22 @@ router.get('/home', function(req, res, next) {
   res.render('main');
 });
 
+/* render signup page */
+router.get('/signup', function(req, res, next) {
+  res.render('signup');
+});
+
+router.post('/signup', function(req, res, next) {
+  res.send('signup POST route not yet completed');
+});
+
+/* render login form */
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.post('/login', function(req, res, next) {
+  res.send('login POST route not yet completed');
+});
+
 module.exports = router;
